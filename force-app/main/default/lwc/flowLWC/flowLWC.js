394f546715183c3643e91427c2a23e26799c175a
goc_vivek <98304998+vivekdevmurari@users.noreply.github.com>
2024-09-11 17:38:49 +0530

    Added flowLWC and sldsStylingLWC

---
+++ force-app/main/default/lwc/flowLWC/flowLWC.js
@@ -0,0 +1,6 @@
+import { LightningElement,api } from 'lwc';
+
+export default class FlowLWC extends LightningElement {
+
+    @api recordId;
+}
---
+++ force-app/main/default/lwc/sldsStylingLWC/sldsStylingLWC.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class SldsStylingLWC extends LightningElement {}
